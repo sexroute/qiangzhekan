408a0c3510addce0a34231c83d72072bee1d1183
sexroute <sexroute@gmail.com>
2014-05-11 16:18:22 +0800

    [ADD] 增加新闻页

---
+++ test/LYNewsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LYNewsTableViewController.h
+//  finger
+//
+//  Created by zhao on 14-5-11.
+//  Copyright (c) 2014年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LYNewsTableViewController : UITableViewController
+
+@end
